888667864ef5fb9c63ffd6da26457d00ef409215
snover096 <ouroborus096@gmail.com>
2026-05-31 00:54:21 +0800

    自动爬下一页版

--- Scraper爬？/scraper_ai.py
+++ Scraper爬？/scraper_ai.py
@@ -2,17 +2,39 @@ import requests
 from bs4 import BeautifulSoup
 import ollama # 导入 ollama 库
 
-def scrape_quotes():
-    url = 'http://quotes.toscrape.com/'
+def scrape_quotes(url):
+    count = 0
+    maxCount = 10
+    goodurl = f"{url}".rstrip('/')
+    all_quotes = ""
+
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
     
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        quotes = soup.find_all('span', class_='text')
-        # 将抓取到的名言合并成一段长文本
-        return " ".join([q.get_text() for q in quotes])
-    return ""
+    
+    while True:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            quotes = soup.find_all('span', class_='text')
+            print('='*50)
+            print(quotes)
+            all_quotes += " ".join([q.get_text() for q in quotes])
+            # print(all_quotes)  
+        else:
+            print("bad url") 
+            break
+
+        nextButton = soup.find('li',"next")
+        if nextButton and count < maxCount:
+            count += 1
+            nextA = nextButton.find('a')['href']
+            url = f"{goodurl}{nextA}"
+            print(url)    
+        else:
+            break
+          
+    return all_quotes    
+        
 
 def analyze_with_local_ai(text):
     print("正在将数据发送给本地大模型进行分析，请稍候...\n")
@@ -28,7 +50,7 @@ def analyze_with_local_ai(text):
     请用中文回答，保持排版清晰。
     """
     
-    # 调用你本地的模型 (请将 'qwen2.5:7b' 替换为你实际在 Ollama 中运行的模型名称)
+    # 调用你本地的模型 (请将 model替换为你实际在 Ollama 中运行的模型名称)
     # 比如 llama3, qwen2.5:14b 等
     response = ollama.chat(model='Gemma4-8b', messages=[
         {'role': 'user', 'content': prompt}
@@ -38,11 +60,13 @@ def analyze_with_local_ai(text):
 
 if __name__ == "__main__":
     # 1. 爬虫抓取
-    scraped_text = scrape_quotes()
-    if scraped_text:
-        # 2. AI 分析
-        ai_result = analyze_with_local_ai(scraped_text)
-        print("=== 本地 AI 分析结果 ===")
-        print(ai_result)
-    else:
-        print("抓取失败")
+    scraped_text = scrape_quotes("http://quotes.toscrape.com/")
+    
+    
+    # if scraped_text:
+    #     # 2. AI 分析
+    #     ai_result = analyze_with_local_ai(scraped_text)
+    #     print("=== 本地 AI 分析结果 ===")
+    #     print(ai_result)
+    # else:
+    #     print("抓取失败")
