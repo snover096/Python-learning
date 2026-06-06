d6894c1eb90224d81ad96f9cefb7d26b80c2f1f9
snover096 <ouroborus096@gmail.com>
2026-06-06 13:03:02 +0800

    。。

--- 经典HelloWorld.py
+++ 经典HelloWorld.py
@@ -4,3 +4,11 @@ print(msg)
 import numpy as np
 
 print(f"here is a random num between 1 and 100: {np.random.randint(1,100)}")
+
+
+
+if __name__ == "__main__": # pylint: disable=C0103
+    print("This script is being run directly.")  
+else :    
+    print ("The module 'HelloWorld.py' has been imported in another program")     
+
